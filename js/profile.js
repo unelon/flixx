9cd59e2fb789660769151f3acd27d57f5b187679
unebune <rlillevang@gmail.com>
2024-07-17 19:20:30 +0200

    refactor profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,12 +1,7 @@
 const apiToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTBiMTUxZGVmYzYyNzI2MDVjZWQxYTZjZDI5YjU3MiIsIm5iZiI6MTcyMDQzODU5NS4xNTA3MDksInN1YiI6IjY2OGJiMmRjMjk5ZGU2Zjk2ODNkMmU4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RdZwp59vudKRBx8xzYKUtHpc97UDMtUfpr6r3lzHgio";
-const searchParams = new URLSearchParams(window.location.search);
-const personId = searchParams.get('id'); 
+const personId = new URLSearchParams(window.location.search).get('id');
 
 const profileUrl = `https://api.themoviedb.org/3/person/${personId}`;
-
-console.log("window location: " + searchParams)
-console.log("Search params: " + searchParams)
-console.log("Profile URL: " + profileUrl)
 
 const getProfile = async () => {
 
