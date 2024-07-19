376a5daf3d16a7aef3ae98b52549bbbca09a29aa
unebune <rlillevang@gmail.com>
2024-07-19 09:52:46 +0200

    profile page

--- js/profile.js
+++ js/profile.js
@@ -2,19 +2,46 @@ const apiToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTBiMTUxZGVmYzYyNzI2MDVjZWQxY
 const personId = new URLSearchParams(window.location.search).get('id');
 
 const profileUrl = `https://api.themoviedb.org/3/person/${personId}`;
+const imageUrl = `https://image.tmdb.org/t/p/w500/`;
+const imdbProfileUrl = "https://www.imdb.com/name/";
+
+// DOM selectors
+const title = document.getElementById("profile-title");
+const content = document.getElementById("content");
+const profileImg = document.getElementById("image");
+const birthday = document.getElementById("birthday");
+const birthplace = document.getElementById("birthplace");
+const imdbProfile = document.getElementById("imdbProfile");
 
 const getProfile = async () => {
 
-    const res = await fetch(profileUrl, {
-        method: "GET",
-        headers: {
-            "content-type": "application/json",
-            Authorization: "Bearer " + apiToken,
-        }
-    })
+    try {
+        const res = await fetch(profileUrl, {
+            method: "GET",
+            headers: {
+                "content-type": "application/json",
+                Authorization: "Bearer " + apiToken,
+            }
+        })
 
-    const data = await res.json();
-    console.log(data)
+        const data = await res.json();
+        console.log(data)
+
+        title.innerText = data.name;
+        if (data.biography !== "") {
+        content.innerText = data.biography
+        } else {
+            content.innerText = "No data available"
+        }
+       
+        profileImg.src = imageUrl + data.profile_path;
+        birthday.innerText = data.birthday;
+        birthplace.innerText = data.place_of_birth;
+        imdbProfile.href = imdbProfileUrl + data.imdb_id
+        
+    } catch (error) {
+        
+    }
 
 }
 
